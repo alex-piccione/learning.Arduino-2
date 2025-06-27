338390a7cf5e699234e523fd225802105bdad8b2
Alex <alessandro.piccione.75@gmail.com>
2025-06-27 16:34:44 +0200

    clean up

--- Projects/03 - NTC and Nokia5110/src/main.cpp
+++ Projects/03 - NTC and Nokia5110/src/main.cpp
@@ -2,18 +2,27 @@
 
 #include <Adafruit_GFX.h>     // Include the Adafruit GFX library
 #include <Adafruit_PCD8544.h> // Include the Adafruit PCD8544 library for Nokia 5110 LCD
+#include <DHT.h>             // La librería principal del DHT
+#include <DHT_U.h>           // La librería de DHT para sensores DHT21, DHT22, etc.
 
 bool logging_enabled = true; // Enable or disable logging to Serial Monitor
 
 // NTC data
 const int ntc_pin = A0; // Pin for the NTC thermistor
-const float ntc_R0 = 8500; // 9325.7; // NTC thermistor resistance at reference temperature (25 degrees Celsius), it should be 10 000.
+const float ntc_R0 = 8300; // 9325.7; // NTC thermistor resistance at reference temperature (25 degrees Celsius), it should be 10 000.
 const float ntc_T0 = 298.15; // Reference temperature in Kelvin (25 degrees Celsius + 273.15)
 const float ntc_BETA = 4288.4; // 3950; // Beta value for the NTC thermistor
 const float ntc_R_ref = 9990; // actual value of the 10K resistence used in the voltage divider (it seems to go up with temperature)
 
 const float VCC = 5; // Voltage Common Collector = Positive alimentation of the circuit (5V for Arduino Uno)
 float adcToVoltageFactor = VCC / 1024; // Factor to convert ADC value to voltage (values goes from 0 to 1023, so it has 1024 steps)
+
+// DHT21 component data
+const int dht_pin = 8;
+#define DHTPIN 8     // Pin digital donde conectaste el cable de datos del DHT
+#define DHTTYPE DHT21 // Tipo de sensor DHT (DHT11, DHT21, DHT22)
+
+DHT dht(DHTPIN, DHTTYPE); // Inicializa el objeto DHT
 
 // Nokia 5110 data
 const int display_pin_RST = 2; // Reset pin for the LCD
@@ -26,6 +35,7 @@ const int display_pin_CLK = 6; // Clock pin for the LCD
 void printText(String text);
 void logValue(String label, float value);
 float calculateTemperature();
+float readTemperature();
 float calculateVoltage(float adcValue);
 
 // define pins of the display, CLK, DIN, DC, CE, RST
@@ -35,11 +45,12 @@ Adafruit_PCD8544 display(display_pin_CLK, display_pin_DIN, display_pin_DC, displ
 #define DISPLAY_WIDTH 84  // Width of the Nokia 5110 LCD in pixels
 #define DISPLAY_HEIGHT 48  // Height of the Nokia 5110 LCD in pixels
 
-//const int ntcPin = A0; // Pin for the NTC thermistor
-//const float beta = 3950.0; // Beta value for the NTC thermistor
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(ntc_pin, INPUT); // Set the NTC pin as input
+  pinMode(dht_pin, INPUT); // Set the DHT21 pin as input
 
   // Initialize the Nokia 5110 LCD
   display.begin(); // initialize SPI communication
@@ -52,9 +63,14 @@ void setup() {
 }
 
 void loop() {  
-  float tempersture = calculateTemperature();
-  printText("Temperature:\n" + String(tempersture, 2) + " " + (char)247 + "C\n\n");
-  delay(1000); // Wait for a second
+  float temperature = calculateTemperature();
+  printText("Temperature:\n" + String(temperature, 2) + " " + (char)247 + "C\n\n");
+  
+  delay(1000); // Wait for a a little bit
+  temperature = readTemperature(); 
+  printText("Temperature:\n" + String(temperature, 2) + " " + (char)247 + "C\n\n");
+  
+  delay(3000); // Wait
 }
 
 void printText(String text) {
@@ -105,4 +121,26 @@ float calculateTemperature() {
 float calculateVoltage(float adcValue) {
   // Convert the ADC value to voltage
   return adcValue * adcToVoltageFactor; // V = ADC value * (VCC / 1024)
+}
+
+float readTemperature() {
+  Serial.println("Reading DHT Sensor...");
+
+  // Leer la humedad
+  //float h = dht.readHumidity();
+  // Leer la temperatura en Celsius
+  float t = dht.readTemperature();
+
+  //if (isnan(h) || isnan(t)) {
+  if (isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    // Puedes mostrar un error en el LCD si quieres
+    // printText("DHT Read Error!");
+    return 0.0; // Retorna 0.0 en caso de error
+  }
+
+  //logValue("Humidity", h);
+  logValue("Temperature", t);
+
+  return t; // Placeholder for actual temperature reading logic
 }
